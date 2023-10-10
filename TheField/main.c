23631a0871dc3f93d4eeb438047222a91a8936a3
Arsenty Halimovsky <ghost12@yandex.ru>
2023-10-10 03:56:37 +0300

    Add multi-dimensional array example

--- TheField/main.c
+++ TheField/main.c
@@ -7,15 +7,27 @@
 
 #include <stdio.h>
 
+int a[2][3]; /* A 2 x 3 array */
+int b[2][3];
+
 int main() {
-    int a[10];
+    
+    int a[2][3] = {
+        {3,2,6},
+        {4,5,20}
+    };
+    
+    int b[2][3] = { {3,2,6}, {4,5,20} };
+    
+    printf("Element 3 in row 2 is %d\n", a[1][2]); /* 20 */
+    a[1][2] = 25;
+    printf("Element 3 in row 2 is %d\n", a[1][2]); /* 25 */
+    
+    /* iteration through two-dimensional array */
     int k;
-    
-    for (k = 0; k < 10; k++) {
-        a[k] = k * 10;
-        printf("new a[k] number is %d \n", a[k]);
-    }
-    
+    int j;
+    /* display array contents */
+        printf("first element is -  %d \n", a[0][0]);
     return 0;
 }
 
