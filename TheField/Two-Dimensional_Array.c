b5c911bf69705cf05b5bdd5a41f3bf87a5a6d6be
Arsenty Halimovsky <ghost12@yandex.ru>
2023-10-11 04:13:11 +0300

    Add TwoDimensionalArray example

---
+++ TheField/Two-Dimensional_Array.c
@@ -0,0 +1,33 @@
+//
+//  Two-Dimensional_Array.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 11.10.2023.
+//
+
+//#include "Two-Dimensional_Array.h"
+// #include <stdio.h>
+//
+//int a[2][3]; /* A 2 x 3 array */
+//int b[2][3];
+//
+//int main() {
+//
+//    int a[2][3] = {
+//        {3,2,6},
+//        {4,5,20}
+//    };
+//
+//    int b[2][3] = { {3,2,6}, {4,5,20} };
+//
+//    printf("Element 3 in row 2 is %d\n", a[1][2]); /* 20 */
+//    a[1][2] = 25;
+//    printf("Element 3 in row 2 is %d\n", a[1][2]); /* 25 */
+//
+//    /* iteration through two-dimensional array */
+//    int k;
+//    int j;
+//    /* display array contents */
+//        printf("first element is -  %d \n", a[0][0]);
+//    return 0;
+//}
---
+++ TheField/Two-Dimensional_Array.h
@@ -0,0 +1,13 @@
+//
+//  Two-Dimensional_Array.h
+//  TheField
+//
+//  Created by Арсентий Халимовский on 11.10.2023.
+//
+
+#ifndef Two_Dimensional_Array_h
+#define Two_Dimensional_Array_h
+
+#include <stdio.h>
+
+#endif /* Two_Dimensional_Array_h */
