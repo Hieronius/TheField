48b34db5e6c2406f4a52cb468c15bc339ab725f9
Arsenty Halimovsky <ghost12@yandex.ru>
2023-10-11 04:48:25 +0300

    Add Pointers

---
+++ TheField/Pointers.c
@@ -0,0 +1,54 @@
+//
+//  Pointers.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 11.10.2023.
+//
+
+#include "Pointers.h"
+
+// int main() {
+//    int j = 63;
+//    int *p = NULL;
+//    p = &j;
+//
+//    printf("The address of j is %x\n", &j);
+//    printf("p contains address %x\n", p);
+//    printf("The value of j is %d\n", j);
+//    printf("p is pointing to the value %d\n", *p);
+    
+//    int x = 12;
+//    int *pt = NULL;
+//    int **ptr = NULL;
+//    pt = &x;
+//    ptr = &pt;
+//
+//    printf("The address of x is %x\n", &x);
+//    printf("pt contains address %x\n", pt);
+//    printf("ptr contains address %x\n", ptr);
+//    printf("The value of x is %d\n", x);
+//    printf("pt is pointing to the value %d\n", *pt);
+//    printf("ptr is pointing to the value %d\n", **ptr);
+    
+//    int x = 5;
+//    int y;
+//    int *p = NULL;
+//    p = &x;
+//    int *c = &x;
+//
+//    y = *p + 2; /* y is assigned 7 */
+//    y += *p; /* y is assigned 12 */
+//    *p = y; /* x is assigned 12 */
+//    (*p)++; /* x is incremented to 13 */
+//
+//    printf("p is pointing to the value %d\n", *p);
+//    printf("c is pointing to the value %d\n", *c);
+//
+//    return 0;
+//}
+
+// Class Pointer
+// var 1 = Pointer()
+// var 2 = Pointer()
+// var 3 = Pointer()
+// All of them have the same address of Reference type Class Pointer
---
+++ TheField/Pointers.h
@@ -0,0 +1,13 @@
+//
+//  Pointers.h
+//  TheField
+//
+//  Created by Арсентий Халимовский on 11.10.2023.
+//
+
+#ifndef Pointers_h
+#define Pointers_h
+
+#include <stdio.h>
+
+#endif /* Pointers_h */
