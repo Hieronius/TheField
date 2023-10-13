d949b7938fc5e2f4e4450636718548b295d91fe7
Arsenty Halimovsky <ghost12@yandex.ru>
2023-10-13 04:12:00 +0300

    Add Swap function

---
+++ TheField/Swap_Function.c
@@ -0,0 +1,44 @@
+//
+//  Swap_Function.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 12.10.2023.
+//
+
+//#include "Swap_Function.h"
+//
+//void swap (int *num1, int *num2);
+//void swap1 (int num3, int num4);
+//
+//int main() {
+//    int x = 25;
+//    int y = 100;
+//    int z = 50;
+//    int b = 150;
+//    
+//    printf("x is %d, y is %d \n", x, y);
+//    swap(&x, &y);
+//    printf("x is %d, y is %d \n", x, y);
+//    
+//    printf("z is %d, b is %d \n", z, b);
+//    swap1(z, b);
+//    printf("z is %d, b is %d \n", z, b);
+//    
+//    return 0;
+//}
+//
+//void swap (int *num1, int *num2) {
+//    int temp;
+//    
+//    temp = *num1;
+//    *num1 = *num2;
+//    *num2 = temp;
+//}
+//
+//void swap1 (int num3, int num4) {
+//    int temp;
+//    
+//    temp = num3;
+//    num3 = num4;
+//    num4 = temp;
+//}
--- TheField/main.c
+++ TheField/main.c
@@ -7,43 +7,9 @@
 
 #include <stdio.h>
 
-int main() {
-    
-}
-
-//
-//int main() {
-//    int a[5] = {22, 33, 44, 55, 66};
-//    int *ptr = NULL;
-//    int i;
-//
-//    ptr = a; /* the same as ptr = &a[0] */
-//    for (i = 0; i < 5; i++) {
-//        printf("%d \n ", *(ptr + i));
-//    }
-//}
 
 
-//int main() {
-//    int a[5] = {22, 33, 44, 55, 66};
-//    int *ptr = NULL;
-//
-////    ptr = a; /* point to the first array element */
-//    printf("%d %x\n", *ptr, ptr);
-//
-//    ptr++;
-//    printf("%d %x\n", *ptr, ptr);
-//
-//    ptr += 3;
-//    printf("%d %x\n", *ptr, ptr);
-//
-//    ptr--;
-//    printf("%d %x\n", *ptr, ptr);
-//
-//    ptr -= 2;
-//    printf("%d %x\n", *ptr, ptr);
-//
-//}
+
 
 
 
