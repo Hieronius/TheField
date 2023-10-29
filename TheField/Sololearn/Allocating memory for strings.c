127031a37393ee240e3baea9c2a867060d35f625
Arsenty Halimovsky <ghost12@yandex.ru>
2023-10-29 04:59:22 +0300

    Add Allocating memory for strings

---
+++ TheField/Sololearn/Allocating memory for strings.c
@@ -0,0 +1,25 @@
+//
+//  Allocating memory for strings.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 29.10.2023.
+//
+
+// MARK: Allocating Memory for Strings
+
+/*
+int main() {
+    char str20[20];
+    char *str = NULL;
+    
+    strcpy(str20, "12345");
+    printf("str20 size: %ld\n", sizeof(str20));
+    printf("str20 lenght %ld\n", strlen(str20));
+    str = malloc(strlen(str20)+1); /* make room for \0
+    strcpy(str, str20);
+    printf("%s", str);
+    
+    return 0;
+}
+
+*/
