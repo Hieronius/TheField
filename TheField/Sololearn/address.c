a2e257d2a3701d96f62165e2831d42c285cc9150
Arsenty Halimovsky <ghost12@yandex.ru>
2023-11-02 03:53:30 +0300

    Add address / pointer explanation

---
+++ TheField/Sololearn/address.c
@@ -0,0 +1,72 @@
+//
+//  address.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 02.11.2023.
+//
+
+#include <stdio.h>
+#define N 16
+
+// MARK: Address
+
+//int main() {
+//    int A[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//    int x = *(A+1); // dereference
+//    printf("%d\n", x);
+//
+//    return 0;
+//}
+
+// MARK: Pointers
+/*
+// argc - "argument count of command line", argv - "arguments data type.
+int main(int argc, char* argv[]) {
+    
+    int i = 10;
+    int *ptr = &i;
+    int **pptr = &ptr;
+    int ***ppptr = &pptr;
+    
+    printf("%d\n", i);
+    *ptr = 20;
+    printf("%d\n", i);
+    **pptr = 30;
+    printf("%d\n", i);
+    ***ppptr = 40;
+    printf("%d\n", i);
+    printf("%d\n", *ptr);
+    printf("%d\n", **pptr);
+    printf("%d\n", ***ppptr);
+    
+    
+    // printf - "print format"
+    //printf("Hello, World\n");
+    // "\n" - символ переноса каретки на старых компьютерах.
+    
+    // declaration of an array property
+//    int A[N] = {0};
+    
+    // basic algorithm to fill an array from last to first element
+//    for (int i = 0; i < N; ++i) {
+//        A[i] = N - i - 1;
+//    }
+    
+    // calculate an index of the current cell and defining i to it.
+    // With this way to do things we fill an array from end to start.
+    // Ability to fil an array with two numbers during each new iteration
+//    for (int i = 0; i < N/2; ++i) {
+//        A[2*i] = i;
+//        A[2*i+1] = N/2 + i;
+//    }
+    
+//    for (int i = 0; i < N/2; ++i) {
+//
+//    }
+    
+//    for (int i = 0; i < N; ++i) {
+//        printf(" %d ", A[i]);
+//    }
+    return 0;
+}
+ */
