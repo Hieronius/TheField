2b708582041ada6acd775ec8cde9fbb7d38ace47
Arsenty Halimovsky <ghost12@yandex.ru>
2023-11-03 11:04:31 +0300

    Add IterateArrayAndMapSquare().c

---
+++ TheField/School21/IterateArrayAndMapSquare().c
@@ -0,0 +1,53 @@
+//
+//  IterateArrayAndMapSquare().c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 03.11.2023.
+//
+
+#include <stdio.h>
+#define NMAX 10
+
+int input(int *a, int *n);
+void output(int *a, int n);
+void squaring(int *a, int n);
+
+int main() {
+    int n;
+    int data[NMAX];
+    if (input(data, &n) == 1) {
+        printf("n/a");
+    } else {
+        squaring(data, n);
+        output(data, n);
+    }
+
+    return 0;
+}
+
+int input(int *a, int *n) {
+    if ((scanf("%d", n) != 1) || (*n < 1) || (*n > 10)) {
+        return 1;
+
+    } else {
+        for (int *p = a; p - a < *n; p++) {
+            if (scanf("%d", p) != 1) {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+void output(int *a, int n) {
+    for (int *p = a; p - a < n; p++) {
+        printf("%d ", *p);
+    }
+}
+
+void squaring(int *a, int n) {
+    for (int *p = a; p - a < n; p++) {
+        *p = *p * *p;
+    }
+}
+
