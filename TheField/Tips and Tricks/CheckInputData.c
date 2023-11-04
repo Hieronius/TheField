683073281eb2e55d2ac1e3415168a7450b9ab4c1
Arsenty Halimovsky <ghost12@yandex.ru>
2023-11-04 04:13:33 +0300

    Add check of input data

---
+++ TheField/Tips and Tricks/CheckInputData.c
@@ -0,0 +1,21 @@
+//
+//  CheckInputData.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 04.11.2023.
+//
+
+#include <stdio.h>
+
+/*
+int input(int *array) {
+   
+    for (i = 0; i < ARRAYMAX; i++) {
+      if (scanf("%d", p) != 1) {
+        return 1;
+      }
+    }
+  }
+  return 0;
+}
+*/
