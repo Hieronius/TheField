70137a34766d9740d0842cda3252dd0ce3ca48f3
Arsenty Halimovsky <ghost12@yandex.ru>
2023-11-10 03:56:15 +0300

    Add door algorithm

---
+++ TheField/School21/T11D17/door_struct.h
@@ -0,0 +1,16 @@
+//
+//  door_struct.h
+//  TheField
+//
+//  Created by Арсентий Халимовский on 09.11.2023.
+//
+
+//#ifndef door_struct_h
+//#define door_struct_h
+//
+//struct doors {
+//    int id;
+//    int status;
+//};
+//
+//#endif /* door_struct_h */
--- TheField/main.c
+++ TheField/main.c
@@ -5,129 +5,52 @@
 //  Created by Арсентий Халимовский on 07.10.2023.
 //
 
-#include <ncurses.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <time.h>
 
-#define N 25  // высота
-#define M 80  // ширина
+// #include "door_struct.h"
 
-int creating_points(int A[N][M], int B[N][M]);
-void rendering(int A[N][M]);
-void initialization(int A[N][M]);
-void update() { printf("\033[H\033[J"); }
+#define DOORS_COUNT 15
+#define MAX_ID_SEED 10000
+
+// Define door struct
+struct door {
+    int id;
+    int status;
+};
+
+void initialize_doors(struct door* doors);
 
 int main() {
-    int A[N][M];
-    int B[N][M];
-    int speed = 1000;
-    int era = 0;
-    int life = 0;
-    initialization(A);
-
-    initscr();
-    cbreak();
-    noecho();
-
-    nodelay(stdscr, TRUE);
-
-    while (life == 0) {
-        printf("ERA: %d\n", era);
-        rendering(A);
-        usleep(speed * 100);
-        update();
-        if (creating_points(A, B) == 1) {
-            printf("GAME OVER\n");
-            printf("LIFE IS OVER");
-            life = 1;
-        }
-        int input = getch();
-        if (input == 'f') {
-            speed -= 200;
-        } else if ((input == 's') && speed < 5000) {
-            speed += 200;
-        } else if (input == 'q') {
-            exit(1);
-        }
-        era += 1;
-    }
-    endwin();
+    // Create an array of struct door with DOORS_COUNT amount of elements
+    struct door doors[DOORS_COUNT];
+    // Use an algorithm to iterate through the array
+    initialize_doors(doors);
+    
     return 0;
 }
 
-int creating_points(int A[N][M], int B[N][M]) {
-    int i1, i3, j1, j3, life = 1;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            if (i == 0) {
-                i1 = N - 1;
-                i3 = i + 1;
-            } else if (i == N - 1) {
-                i1 = i - 1;
-                i3 = 0;
-            } else {
-                i1 = i - 1;
-                i3 = i + 1;
-            }
-
-            if (j == 0) {
-                j1 = M - 1;
-                j3 = j + 1;
-            } else if (j == M - 1) {
-                j1 = j - 1;
-                j3 = 0;
-            } else {
-                j1 = j - 1;
-                j3 = j + 1;
-            }
-
-            int sum01 =
-                A[i1][j1] + A[i1][j] + A[i1][j3] + A[i][j1] + A[i][j3] + A[i3][j1] + A[i3][j] + A[i3][j3];
-            if ((sum01 == 2 && A[i][j] == 1) || (sum01 == 3)) {
-                B[i][j] = 1;
-                life = 0;
-            } else {
-                B[i][j] = 0;
+void initialize_doors(struct door* doors) {
+    
+    srand(time(0));
+    /// seed - it's a number (or vector) used to initialize a pseudorandom number generator
+    int seed = rand() % MAX_ID_SEED;
+    // An algorithm to iterate through an array of doors
+    for (int i = 0; i < DOORS_COUNT; i++) {
+        doors[i].id = (i + seed) % DOORS_COUNT;
+        doors[i].status = 0;
+        printf("%d, %d\n", doors[i].id, doors[i].status);
+    }
+    for (int i = 0; i < DOORS_COUNT; i++) {
+        for (int j = 0; j < DOORS_COUNT; j++) {
+            if (doors[i].id > doors[j].id) {
+                int copy = doors[i].id;
+                doors[i].id = doors[j].id;
+                doors[j].id = copy;
             }
         }
-    }
-
-    // Запись расчётов в матрицу А из матрицы В
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            A[i][j] = B[i][j];
-        }
-    }
-    return life;
-}
-
-void initialization(int A[N][M]) {
-    // Заполнение массива информацией из файла    `
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            char buf;
-            if ((buf = getchar()) != EOF) {
-                A[i][j] = buf % 2;
-            }
-        }
-    }
-    FILE* fp = freopen("/dev/tty", "r", stdin);
-    if (fp == NULL) {
-        exit(1);
     }
 }
 
-void rendering(int A[N][M]) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            if (A[i][j] == 1) {
-                printf("#");
-            } else {
-                printf(" ");
-            }
-        }
-        printf("\n\r");
-    }
-    fflush(stdout);
-}
+
