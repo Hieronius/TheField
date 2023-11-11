8e2ada1e0b8a12931c69deabd31a88bc667fdd15
Arsenty Halimovsky <ghost12@yandex.ru>
2023-11-11 04:26:52 +0300

    Add task2

---
+++ TheField/School21/T11D17/Task2.c
@@ -0,0 +1,15 @@
+//
+//  Task2.c
+//  School21_Practice
+//
+//  Created by Voldemort Neda on 10/26/23.
+//
+
+//#include <stdio.h>
+//
+//int main() {
+//  int name;
+//  scanf("%d", &name);
+//  printf("Hello, %d!", name);
+//  return 0;
+//}
