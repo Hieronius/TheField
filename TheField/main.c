24d61da84484472dc2c3f87f6716dbebf51eacd4
Arsenty Halimovsky <ghost12@yandex.ru>
2023-11-12 04:11:09 +0300

    Simple realisation of polish notation

--- TheField/main.c
+++ TheField/main.c
@@ -6,9 +6,39 @@
 //
 
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 
 int main() {
-    int a = 1;
-    int b = 2;
-    int c = +ab;
+    int i;
+    int n;
+    // Function generating random data
+    for (i = 0; i < n; i++){
+        float random = ((float)rand() / (float)(RAND_MAX));
+        float_arr[i] = random;
+        printf("%f  ", random);
+    }
+    //Dividing float data into bins
+    for (i = 0; i < n; i++){
+        for (j = 1; j <= bins; j++){
+
+            float bin_max = (float)j / (float)bins;
+            if (float_arr[i] <= bin_max){
+                hist[j]++;
+                break;
+            }
+        }
+    }
+    // Plotting histogram
+    printf("\n\nHistogram of Float data\n");
+    for (i = 1; i <= bins; i++)
+    {
+        count = hist[i];
+        printf("0.%d |", i - 1);
+        for (j = 0; j < count; j++)
+        {
+            printf("%c", (char)254u);
+        }
+        printf("\n");
+    }
 }
