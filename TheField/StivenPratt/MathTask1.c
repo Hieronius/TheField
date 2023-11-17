b19b59de87ae62971ff5c7fbd24c1f1cef4619c5
Arsenty Halimovsky <ghost12@yandex.ru>
2023-11-17 04:02:21 +0300

    Add math task 1 from Stiven Pratt

---
+++ TheField/StivenPratt/MathTask1.c
@@ -0,0 +1,22 @@
+//
+//  MathTask1.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 17.11.2023.
+//
+
+//int main() {
+//    float weight; // weight of the user
+//    float value; // platinum equity
+//
+//    printf("Do you want to know your weight in platinum equvalent?\n");
+//    printf("Let's count.\n)");
+//    printf("Please, text input your weight, in lbs: ");
+//
+//    /* get input data from the user */
+//    scanf("%f", &weight);
+//    // Let's count - 1700 backs for portion
+//    // 14.5833 is a ratio for weight transfer from lbs to ounces
+//    value = 1700.0 * weight * 14.5833;
+//    printf("Your weight in platinum equvalent is $%.2f.\n", value);
+//}
