2b77f55aac5bd006ecb7ec011615d0397569e418
Arsenty Halimovsky <ghost12@yandex.ru>
2023-11-18 05:43:09 +0300

    Add 4.1 task from Stepik

---
+++ TheField/Stepik/4_1.c
@@ -0,0 +1,17 @@
+//
+//  4.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 18.11.2023.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int A;
+    scanf("%d", &A);
+    double R = (M_PI/180) * A;
+    printf("%.2lf", R);
+    return 0;
+}
