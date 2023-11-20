14eb79afdf5268b8042c4e90ea914abd018703b2
Arsenty Halimovsky <ghost12@yandex.ru>
2023-11-20 05:43:39 +0300

    Add 4.2 task from Stepik

---
+++ TheField/Stepik/4_2.c
@@ -0,0 +1,19 @@
+//
+//  4_2.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 18.11.2023.
+//
+
+//#include <stdio.h>
+//
+//int main() {
+//    int a;
+//    scanf("%d", &a);
+//    if (a % 2 == 0) {
+//        printf("0");
+//    } else {
+//        printf("1");
+//    }
+//    return 0;
+//}
