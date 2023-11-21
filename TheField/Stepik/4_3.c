eb73a2cb0082e81ab51354156f5d8f6172251868
Arsenty Halimovsky <ghost12@yandex.ru>
2023-11-21 03:53:38 +0300

    Add 4.3 task from Stepik

---
+++ TheField/Stepik/4_3.c
@@ -0,0 +1,21 @@
+//
+//  4_3.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 18.11.2023.
+//
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main() {
+//    int x1;
+//    int x2;
+//    scanf("%d", &x1);
+//    scanf("%d", &x2);
+//    int result;
+//    
+//    result = abs(x2 - x1);
+//    printf("%d", result);
+//    return 0;
+//}
