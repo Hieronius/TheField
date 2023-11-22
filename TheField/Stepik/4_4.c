f5370b7a4d8dbdfa946f769eaa3b57e2d0e21330
Arsenty Halimovsky <ghost12@yandex.ru>
2023-11-22 03:43:48 +0300

    Add 4.4 task from stepik

---
+++ TheField/Stepik/4_4.c
@@ -0,0 +1,24 @@
+//
+//  4_4.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 18.11.2023.
+//
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <math.h>
+//
+//int main() {
+//    double x1;
+//    double y1;
+//    double x2;
+//    double y2;
+//    scanf("%lf%lf%lf%lf", &x1, &y1, &x2, &y2);
+//
+//     double result = sqrt(pow((x2 - x1),2) + pow((y2 - y1),2));
+//    // formula of the distance between two points
+//    // sqrt(paw((x2 - x1), 2) + paw((y2 - y1))
+//    printf("%.2lf", result);
+//    return 0;
+//}
---
+++ TheField/Stepik/5_2.c
@@ -0,0 +1,25 @@
+//
+//  5_2.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 21.11.2023.
+//
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <math.h>
+//
+//int main() {
+//    int h;
+//    int r;
+//    double pi = 3.14159265358979323846;
+//
+//    scanf("%d %d", &h, &r);
+//    printf("%d\n", r * r);
+//
+//    double cylinderVolume = pi * (r*r) * h;
+//    double coneVolume = (pi * (r*r) * h) / 3;
+//
+//    printf("%.2lf %.2lf", cylinderVolume, coneVolume);
+//
+//}
