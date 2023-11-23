49209b9d8e9eb0956c21d895ea9856dccd255c20
Arsenty Halimovsky <ghost12@yandex.ru>
2023-11-23 03:35:34 +0300

    Add 4_5 task from Stepik

---
+++ TheField/Stepik/4_5.c
@@ -0,0 +1,24 @@
+//
+//  4_5.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 19.11.2023.
+//
+
+//#include <stdio.h>
+//#include <math.h>
+//#include <stdlib.h>
+//
+//int main() {
+//    double a;
+//    double b;
+//    double y;
+//    double rad;
+//    double result;
+//    
+//    scanf("%lf%lf%lf", &a, &b, &y);
+//    rad = y * (M_PI/180);
+//    result = a * b * (sin(rad) / 2);
+//    printf("%.2lf", result);
+//    return 0;
+//}
