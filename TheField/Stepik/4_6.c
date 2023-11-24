03a6f04a2a591e156568c82894e634dd4469000d
Arsenty Halimovsky <ghost12@yandex.ru>
2023-11-24 04:11:10 +0300

    Add 4.6 task from Stepik

---
+++ TheField/Stepik/4_6.c
@@ -0,0 +1,23 @@
+//
+//  4_6.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 19.11.2023.
+//
+
+//#include <stdio.h>
+//#include <math.h>
+//#include <stdlib.h>
+//
+//int main() {
+//    int N;
+//    int result;
+//    
+//    scanf("%d", &N);
+//    
+//    result = pow(2, N);
+//    
+//    printf("%d", result);
+//    
+//    return 0;
+//}
