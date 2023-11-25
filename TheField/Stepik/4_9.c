3f05a05797f9bad57dfdaf9b8a287001301a2fb8
Arsenty Halimovsky <ghost12@yandex.ru>
2023-11-25 03:56:47 +0300

    Add 4.9 task from Stepik

---
+++ TheField/Stepik/4_9.c
@@ -0,0 +1,26 @@
+//
+//  4_9.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 20.11.2023.
+//
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <time.h>
+//
+//int random_int(int min, int max);
+//
+//int main() {
+//    int S;
+//    int E;
+//    scanf("%d%d", &S, &E);
+//    // int rand_N = S + rand() % E;
+//    int rand_N = random_int(S, E);
+//    printf("%d", rand_N);
+//    return 0;
+//}
+//
+//int random_int(int min, int max) {
+//    return min + rand() % (max+1 - min);
+//}
