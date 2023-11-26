98b7f18ddf9bee2892f7bc88c10fffe0a3a0435b
Arsenty Halimovsky <ghost12@yandex.ru>
2023-11-26 03:31:58 +0300

    Add 5.1 task from Stepik

---
+++ TheField/Stepik/5_1.c
@@ -0,0 +1,21 @@
+//
+//  5_1.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 20.11.2023.
+//
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <math.h>
+//
+//int main() {
+//    int x; // edge of the square
+//    scanf("%d", &x);
+//
+//    int areaOfSquare = pow(x, 2); // pow(a, 2)
+//    int areaOfSurface = 6 * (pow(x, 2)); // 6 * pow(a, 2)
+//    int areaOfEdge = pow(x, 3); // pow(a, 3)
+//
+//    printf("%d %d %d", areaOfSquare, areaOfSurface, areaOfEdge);
+//}
