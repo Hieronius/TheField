32b9861ee82854dc4a480a2d8f7db3fcd535a10b
Arsenty Halimovsky <ghost12@yandex.ru>
2023-11-28 04:28:20 +0300

    Add 4.33 task from Stepik

---
+++ TheField/Stepik/CourseMFT/4_33.c
@@ -0,0 +1,25 @@
+//
+//  4_33.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 19.11.2023.
+//
+
+//#include <stdio.h>
+//#include <math.h>
+//#include <stdlib.h>
+//
+//int main() {
+//    double a;
+//    double b;
+//    double c;
+//    double s;
+//    double area;
+//    
+//    scanf("%lf%lf%lf", &a, &b, &c);
+//    s = (a + b + c) / 2;
+//    area = sqrt(s * (s - a) * (s - b) * (s - c));
+//    printf("%.2lf", area);
+//    
+//    return 0;
+//}
