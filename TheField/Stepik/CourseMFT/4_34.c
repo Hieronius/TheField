f67d4a42d71fda28ee875da10a118f5d27b0f09f
Arsenty Halimovsky <ghost12@yandex.ru>
2023-11-29 04:32:43 +0300

    Add 4.34 task from Stepik

---
+++ TheField/Stepik/CourseMFT/4_34.c
@@ -0,0 +1,27 @@
+//
+//  4_34.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 19.11.2023.
+//
+
+//#include <stdio.h>
+//#include <math.h>
+//#include <stdlib.h>
+//
+//int main() {
+//    int a;
+//    int b;
+//    int c;
+//    int d;
+//    int e;
+//    int f;
+//    int h;
+//    int result;
+//    
+//    scanf("%d%d%d%d%d%d%d", &a, &b, &c, &d, &e, &f, &h);
+//    
+//    // result = (double)a / ((double)b * (((double)c / (double)d * ((double)e / ((double)f * (double)h)))));
+//    result = (double)(a/ (b * (c / (d * (e / (f * h))))));
+//    printf("%.2lf", (double)result);
+//}
