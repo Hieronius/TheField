067c23367e88d9aa3eb89f10b6564a898b3a80eb
Arsenty Halimovsky <ghost12@yandex.ru>
2023-12-02 04:00:17 +0300

    Add 1_56 task from Stepik

---
+++ TheField/Stepik/CourseWithTheCat/1_56.c
@@ -0,0 +1,42 @@
+//
+//  1_56.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 28.11.2023.
+//
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <math.h>
+//
+//int readInt();
+//int is_prime(int i);
+//
+//int main() {
+//    int i = readInt();
+//    is_prime(i);
+//    return 0;
+//}
+//
+//int readInt() {
+//    int res;
+//    scanf("%d", &res);
+//    return res;
+//}
+//
+//int is_prime(int i) {
+//    if (i <= 1) {
+//        return 0;
+//    } else if (i == 2) {
+//        printf("1");
+//        return 1;
+//    } else {
+//        if (!(i % 2 == 0)) {
+//            printf("1");
+//            return 1;
+//        } else {
+//            printf("0");
+//            return 0;
+//        }
+//    }
+//}
