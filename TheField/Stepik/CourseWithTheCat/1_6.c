fa6a24c4b7711da9575cde6f4031615f5d3013b1
Arsenty Halimovsky <ghost12@yandex.ru>
2023-12-03 04:03:31 +0300

    Add 1.6 task from Stepik

---
+++ TheField/Stepik/CourseWithTheCat/1_6.c
@@ -0,0 +1,33 @@
+//
+//  1_6.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 29.11.2023.
+//
+
+#include <stdio.h>
+
+//void normilize(int* a);
+//int getInt();
+//
+//int main() {
+//    int a = getInt();
+//    normilize(&a);
+//    printf("%d", a);
+//    return 0;
+//}
+//
+//void normilize(int* a) {
+//    int temp = *a;
+//    while ((temp > 0) && (temp % 2 == 0)) {
+//        temp /= 2;
+//    }
+//    *a = temp;
+//    return;
+//}
+//
+//int getInt() {
+//    int temp;
+//    scanf("%d", &temp);
+//    return temp;
+//}
