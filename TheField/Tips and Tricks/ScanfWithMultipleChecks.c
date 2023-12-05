feea283599f3c1507b1dce2b26f775588dce6949
Arsenty Halimovsky <ghost12@yandex.ru>
2023-12-05 04:02:38 +0300

    Add scanf method with multiple conditions

---
+++ TheField/Tips and Tricks/ScanfWithMultipleChecks.c
@@ -0,0 +1,14 @@
+//
+//  ScanfWithMultipleChecks.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 29.11.2023.
+//
+
+#include <stdio.h>
+
+//int getInt() {
+//    int temp;
+//    scanf("%d", &temp);
+//    return temp;
+//}
