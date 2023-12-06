0b4cb63574e896a4d7981fced9af6e45b552c715
Arsenty Halimovsky <ghost12@yandex.ru>
2023-12-06 04:02:27 +0300

    Add method to reverse the content of an array

---
+++ TheField/Tips and Tricks/ReverseArrayWithPointers.c
@@ -0,0 +1,21 @@
+//
+//  ReverseArrayWithPointers.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 01.12.2023.
+//
+
+//void reverseArray(int* array, int size) {
+//    int newArray[size];
+//
+//    int j = 0;
+//    for (int i = size - 1; i >= 0; i = i - 1) {
+//        newArray[j] = array[i];
+//        j += 1;
+//    }
+//    
+//    for (int i = 0; i < size; i++) {
+//        array[i] = newArray[i];
+//    }
+//    
+//}
