ac615313a5884e9691e899c5a530ffc4cf878666
Arsenty Halimovsky <ghost12@yandex.ru>
2023-12-07 03:45:07 +0300

    Add method to print any arguments

---
+++ TheField/Tips and Tricks/PrintfOfAnyArguments.c
@@ -0,0 +1,15 @@
+//
+//  PrintfOfAnyArguments.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 30.11.2023.
+//
+
+#include <stdio.h>
+
+//void print_int(int arg) {
+//    int res = arg;
+//    printf("%d", arg);
+//    // there can be any variations of the arguments types.
+//    // Just modify any of them.
+//}
