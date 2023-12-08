11e2f2b1a2e58331f3dde42567adf17c5c3ea755
Arsenty Halimovsky <ghost12@yandex.ru>
2023-12-08 03:23:45 +0300

    Add method to create an array with sizeof

---
+++ TheField/Tips and Tricks/HowToCreateAnArrayWithSizeOf.c
@@ -0,0 +1,25 @@
+//
+//  HowToCreateAnArrayWithSizeOf.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 30.11.2023.
+//
+
+#include <stdio.h>
+
+//int main() {
+//    int myArray[] = { 1, 2, 3, 4, 5};
+//    int myArraySize = 5;
+//    f(myArray, myArraySize);
+//    
+//    // p[i] mean the same as *(p + i);
+//    // &p[i] mean the same as P + i;
+//    
+//    return 0;
+//}
+//
+//void f(int* array, int size) {
+//    for (int i = 0; i < size; i = i + 1) {
+//        printf("%d", array[i]);
+//    }
+//}
