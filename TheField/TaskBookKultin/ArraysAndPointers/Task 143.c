b53a8716d192f9e5f1452b8aa081157dd359b2c1
Arsenty Halimovsky <ghost12@yandex.ru>
2023-12-09 03:40:11 +0300

    Add task 143

---
+++ TheField/TaskBookKultin/ArraysAndPointers/Task 143.c
@@ -0,0 +1,27 @@
+//
+//  Task 143.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 08.12.2023.
+//
+
+//#include <stdio.h>
+//#define SIZE 5
+//
+//// MARK: TASK № 143
+//
+//int main() {
+//    int array[SIZE];
+//    int i;
+//    int total = 0;
+//    
+//    for (i = 0; i < SIZE; i++) {
+//        printf("Current element index is - %d\n", i);
+//        scanf("%d", &array[i]);
+//        if (array[i] > 0) {
+//            total += 1;
+//        }
+//    }
+//    printf("Total amount of non zero elements is %d\n", total);
+//    return 0;
+//}
