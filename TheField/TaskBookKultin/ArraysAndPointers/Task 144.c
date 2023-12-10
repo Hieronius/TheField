72382fb5ee66166dad07c594b1f4b14c3041aff6
Arsenty Halimovsky <ghost12@yandex.ru>
2023-12-10 03:25:06 +0300

    Add task 144

---
+++ TheField/TaskBookKultin/ArraysAndPointers/Task 144.c
@@ -0,0 +1,32 @@
+//
+//  Task 144.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 08.12.2023.
+//
+
+// MARK: TASK 144
+
+//#include <stdio.h>
+//#define SIZE 5
+//
+//int main() {
+//    int array[SIZE];
+//    int i;
+//    int min;
+//    
+//    for (i = 0; i < SIZE; i++) {
+//        scanf("%d", &array[i]);
+//    }
+//    
+//    min = array[0];
+//    
+//    for (i = 1; i < SIZE; i++) {
+//        if (array[i] < min) {
+//            min = array[i];
+//        }
+//    }
+//    
+//    printf("The minimum value is %d\n", min);
+//    return 0;
+//}
