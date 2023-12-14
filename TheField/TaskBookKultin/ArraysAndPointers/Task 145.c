990c5dab5c5591591e3e7498562be075349f7c6b
Arsenty Halimovsky <ghost12@yandex.ru>
2023-12-14 10:44:00 +0300

    Add task 145

---
+++ TheField/TaskBookKultin/ArraysAndPointers/Task 145.c
@@ -0,0 +1,33 @@
+//
+//  145.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 13.12.2023.
+//
+
+// MARK: TASK № 145
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#define SIZE 5
+//
+//int main() {
+//    int array[SIZE];
+//
+//    printf("Searching for the minimum element of the array\n");
+//    printf("Enter 5 numbers in the one line and press <Enter>\n");
+//
+//    for (int i = 0; i < SIZE; i++) {
+//        scanf("%d", &array[i]);
+//    }
+//
+//    for (int i = 0; i < SIZE; i++) {
+//        if (*array > *(array + i)) {
+//            int temp = *array;
+//            *array = *(array + i);
+//            *(array + i) = temp;
+//        }
+//    }
+//
+//    printf("Minimum element of the array: %d\n", *array);
+//}
