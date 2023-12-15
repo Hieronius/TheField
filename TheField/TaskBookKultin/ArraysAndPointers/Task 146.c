aa26130701772fd72be438015d49d1d74dac12ca
Arsenty Halimovsky <ghost12@yandex.ru>
2023-12-15 03:39:42 +0300

    Add task 146

---
+++ TheField/TaskBookKultin/ArraysAndPointers/Task 146.c
@@ -0,0 +1,38 @@
+//
+//  Task 146.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 13.12.2023.
+//
+
+// MARK: TASK № 146
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#define SIZE 10
+//
+//int main() {
+//    int array[SIZE];
+//    int sum = 0;
+//    int nonZeroElements = 0;
+//
+//    printf("Enter 10 elements of array in one line and press <Enter>\n");
+//
+//    for (int i = 0; i < SIZE; i++) {
+//        scanf("%d", (array + i));
+//    }
+//
+//    for (int i = 0; i < SIZE; i++) {
+//        sum += *(array + i);
+//
+//        if (*(array + i) != 0) {
+//            nonZeroElements += 1;
+//        }
+//    }
+//
+//    printf("Sum of all elements in array: %d\n", sum);
+//    printf("Amount of non-zero elements: %d\n", nonZeroElements);
+//    printf("Averege non-zero value: %lf\n", (double)sum / (double)nonZeroElements);
+//
+//    return 0;
+//}
