5199bc4e5e79d560384eb7b88139722f833c505e
Arsenty Halimovsky <ghost12@yandex.ru>
2023-12-18 03:58:34 +0300

    Add task 149

---
+++ TheField/TaskBookKultin/ArraysAndPointers/Task 149.c
@@ -0,0 +1,43 @@
+//
+//  Task 149.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 14.12.2023.
+//
+
+// MARK: TASK 149
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <stdbool.h>
+//#define SIZE 5
+//
+//int main() {
+//    int array[SIZE];
+//    int num;
+//    int flag = false;
+//
+//    printf("Enter an array of numbers:\n");
+//
+//    for (int i = 0; i < SIZE; i++) {
+//        scanf("%d", array + i);
+//    }
+//
+//    printf("Enter the number to check:");
+//
+//    scanf("%d", &num);
+//
+//    for (int i = 0; i < SIZE; i++) {
+//        if (*(array + i) == num) {
+//            flag = true;
+//        }
+//    }
+//
+//    if (flag) {
+//        printf("%d number has been found in the array", num);
+//    } else {
+//        printf("%d number hasn't been found in the array", num);
+//    }
+//
+//    return 0;
+//}
