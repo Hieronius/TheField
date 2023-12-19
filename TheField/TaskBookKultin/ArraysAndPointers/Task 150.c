a62e0f20a5b3af74fc09fe74fc84c07071907d8a
Arsenty Halimovsky <ghost12@yandex.ru>
2023-12-19 03:52:01 +0300

    Add Task 150

---
+++ TheField/TaskBookKultin/ArraysAndPointers/Task 150.c
@@ -0,0 +1,44 @@
+//
+//  Task 150.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 15.12.2023.
+//
+
+// MARK: TASK 150
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <stdbool.h>
+//#define SIZE 5
+//
+//int main() {
+//    int array[SIZE];
+//    int isPositive = true;
+//    
+//    printf("Enter an array of numbers\n");
+//    
+//    for (int i = 0; i < SIZE; i++) {
+//        scanf("%d", array + i);
+//    }
+//    
+//    printf("Display an array of numbers\n");
+//    
+//    for (int i = 0; i < SIZE; i++) {
+//        printf("%d\n", *(array + i));
+//        if (*(array + i) > *(array + (i + 1)) && (i < SIZE - 1)) {
+//            isPositive = false;
+//            printf("Found a problem");
+//        }
+//    }
+//    
+//    if (isPositive) {
+//        printf("It's a positive sequence");
+//    } else {
+//        printf("It's a negative sequence");
+//    }
+//    
+//    printf("%d", isPositive);
+//    
+//    return 0;
+//}
