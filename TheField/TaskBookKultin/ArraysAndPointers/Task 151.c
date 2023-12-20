02ee0c7550494224352c14db23f14aa9db37d305
Arsenty Halimovsky <ghost12@yandex.ru>
2023-12-20 03:44:29 +0300

    Add Task 151

---
+++ TheField/TaskBookKultin/ArraysAndPointers/Task 151.c
@@ -0,0 +1,42 @@
+//
+//  Task 151.c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 15.12.2023.
+//
+
+// MARK: TASK 151
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define SIZE 10
+
+//int main() {
+//    int array[SIZE];
+//    int num;
+//    int counter = 0;
+//    
+//    printf("Enter an array of 10 numbers:\n");
+//    
+//    for (int i = 0; i < SIZE; i++) {
+//        scanf("%d", array + i);
+//    }
+//    
+//    printf("Enter the number to check:\n");
+//    
+//    scanf("%d", &num);
+//    
+//    printf("Check the content of the array\n");
+//    
+//    for (int i = 0; i < SIZE; i++) {
+//        printf("%d\n", *(array + i));
+//        if ((*(array + i)) == num) {
+//            counter += 1;
+//        }
+//    }
+//    
+//    printf("%d number has been found %d times\n", num, counter);
+//    
+//    return 0;
+//}
