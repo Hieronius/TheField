9873153a558fc3cffda4d0cff074f8ce9178fd54
Arsenty Halimovsky <ghost12@yandex.ru>
2024-01-08 15:46:10 +0300

    Add 158 task

---
+++ TheField/TaskBookKultin/ArraysAndPointers/Task 158 (Undone).c
@@ -0,0 +1,49 @@
+//
+//  Task 158 (Undone).c
+//  TheField
+//
+//  Created by Арсентий Халимовский on 24.12.2023.
+//
+
+// MARK: TASK 158
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#define N 2
+#define SIZE 5
+
+//int main() {
+//    int array[N][SIZE];
+//    int tempArray[SIZE];
+//    
+//    printf("Enter a first array\n");
+//    
+//    for (int i = 0; i < 1; i++) {
+//        for (int j = 0; j < SIZE; j++) {
+//            scanf("%d", *(array + i) + j);
+//        }
+//    }
+//    
+//    printf("Enter a second array\n");
+//    
+//    for (int i = 1; i < 2; i++) {
+//        for (int j = 0; j < SIZE; j++) {
+//            scanf("%d", *(array + i) + j);
+//        }
+//    }
+//    
+//    printf("Print the content of the array\n");
+//    
+//    for (int i = 0; i < N; i++) {
+//        for (int j = 0; j < SIZE; j++) {
+//            printf("%d ", *(*(array + i) + j));
+//        }
+//        printf("\n");
+//    }
+//    
+//    
+//    
+//    return 0;
+//}
